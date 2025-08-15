8d638cc4ff7d4ada76ab5da5838b003a73f316fc
str0ngh34rt <strongheart@strongheart.games>
2025-08-15 14:25:54 -0400

    Send helpful message when async or unkown chat message detectect

--- ChatCommandHelper/ChatCommandHelper.cs
+++ ChatCommandHelper/ChatCommandHelper.cs
@@ -1,5 +1,12 @@
-﻿namespace ChatCommandHelper {
+﻿using System.Collections.Generic;
+
+namespace ChatCommandHelper {
   public class ChatCommandHelper : IModApi {
+    public static readonly List<string> AsyncChatCommands = new List<string> {
+      "link", "vote", "balance", "claim", "confirmTransfer", "grantCurrency", "revokeCurrency", "shop", "store", "topCurrency",
+      "transfer", "daily", "streak", "topstreak"
+    };
+
     public void InitMod(Mod _modInstance) {
       ModEvents.ChatMessage.RegisterHandler(HandleChatMessage);
     }
@@ -8,7 +15,16 @@
       // This should be the very last mod loaded and so the last handler in the chain, therefore, anything not handled
       // but looking like a command should be treated as such and not sent to the "recipient", but logged.
       if (_data.Message.StartsWith("/")) {
-        // TODO: Differentiate between Async command processing and unknown commands, send a helpful whisper for the latter
+        var whisper = AsyncChatCommands.ContainsCaseInsensitive(_data.Message.Substring(1))
+          ? $"Processing command: {_data.Message}"
+          : $"Unrecognized command: {_data.Message}";
+
+        // Use the net package's logic to determine whether to send the message to the client or not
+        NetPackageChat package = NetPackageManager.GetPackage<NetPackageChat>().Setup(EChatType.Whisper, -1, whisper,
+          new List<int> { _data.SenderEntityId }, EMessageSender.None,
+          GeneratedTextManager.BbCodeSupportMode.Supported);
+        package.ProcessPackage(GameManager.Instance.World, GameManager.Instance);
+
         // Takaro can see messages that get handled by other mods, so it's okay to stop handling its commands
         return ModEvents.EModEventResult.StopHandlersAndVanilla;
       }
