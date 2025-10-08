42c4f8fc1b06c7e510b4f3474386554c49017c5c
str0ngh34rt <strongheart@strongheart.games>
2025-10-08 12:53:41 -0400

    Separate QuestUnlockPOI fixes into its own mod

---
+++ QuestUnlockFixes/QuestUnlockFixes.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace QuestUnlockFixes {
+  [HarmonyPatch(typeof(QuestEventManager), nameof(QuestEventManager.QuestUnlockPOI))]
+  public class QuestEventManager_QuestUnlockPOI_Patch {
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+      CodeMatcher codeMatcher = new(instructions);
+      codeMatcher
+        .MatchStartForward(
+          CodeMatch.LoadsLocal(name: "prefabFromWorldPos")
+        )
+        .ThrowIfInvalid("[StrongUtils] Could not find load instruction");
+      CodeInstruction load = codeMatcher.Instruction;
+      codeMatcher
+        .MatchEndForward(
+          CodeMatch.LoadsField(typeof(PrefabInstance).GetField(nameof(PrefabInstance.lockInstance))),
+          CodeMatch.Branches()
+        )
+        .ThrowIfInvalid("[StrongUtils] Could not find branch instruction");
+      CodeInstruction branch = codeMatcher.Instruction;
+      codeMatcher
+        .MatchStartBackwards(
+          CodeMatch.StoresLocal("prefabFromWorldPos")
+        )
+        .ThrowIfInvalid("[StrongUtils] Could not find insertion point")
+        .Advance(1)
+        .Insert(
+          load,
+          branch
+        );
+      //Log.Out($"[StrongUtils] QuestUnlockPOI instructions:\n    {string.Join("\n    ", codeMatcher.Instructions())}");
+      return codeMatcher.Instructions();
+    }
+
+    private static void Prefix(int entityID, Vector3 prefabPos) {
+      PrefabInstance prefabFromWorldPos = GameManager.Instance.GetDynamicPrefabDecorator()
+        .GetPrefabFromWorldPos((int)prefabPos.x, (int)prefabPos.z);
+      if (prefabFromWorldPos is null) {
+        Log.Error(
+          $"[StrongUtils] Could not find prefabFromWorldPos, skipping: entityID: {entityID} prefabPos: {prefabPos}\n{Environment.StackTrace}");
+      }
+    }
+  }
+
+  public class QuestUnlockFixes : IModApi {
+    public void InitMod(Mod _modInstance) {
+      Harmony harmony = new(_modInstance.Name);
+      harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+  }
+}
