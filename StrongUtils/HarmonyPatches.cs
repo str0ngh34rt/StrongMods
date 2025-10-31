ea97fa84d4e039887e21514f6559796ec6e8962a
str0ngh34rt <strongheart@strongheart.games>
2025-10-31 15:43:00 -0400

    Remove old QuestEventManager patch

--- StrongUtils/HarmonyPatches.cs
+++ StrongUtils/HarmonyPatches.cs
@@ -26,47 +26,6 @@ namespace StrongUtils {
     }
   }
 
-  /*[HarmonyPatch(typeof(QuestEventManager), nameof(QuestEventManager.QuestUnlockPOI))]
-  public class QuestEventManager_QuestUnlockPOI_Patch {
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-      CodeMatcher codeMatcher = new(instructions);
-      codeMatcher
-        .MatchStartForward(
-          CodeMatch.LoadsLocal(name: "prefabFromWorldPos")
-        )
-        .ThrowIfInvalid("[StrongUtils] Could not find load instruction");
-      CodeInstruction load = codeMatcher.Instruction;
-      codeMatcher
-        .MatchEndForward(
-          CodeMatch.LoadsField(typeof(PrefabInstance).GetField(nameof(PrefabInstance.lockInstance))),
-          CodeMatch.Branches()
-        )
-        .ThrowIfInvalid("[StrongUtils] Could not find branch instruction");
-      CodeInstruction branch = codeMatcher.Instruction;
-      codeMatcher
-        .MatchStartBackwards(
-          CodeMatch.StoresLocal("prefabFromWorldPos")
-        )
-        .ThrowIfInvalid("[StrongUtils] Could not find insertion point")
-        .Advance(1)
-        .Insert(
-          load,
-          branch
-        );
-      //Log.Out($"[StrongUtils] QuestUnlockPOI instructions:\n    {string.Join("\n    ", codeMatcher.Instructions())}");
-      return codeMatcher.Instructions();
-    }
-
-    private static void Prefix(int entityID, Vector3 prefabPos) {
-      PrefabInstance prefabFromWorldPos = GameManager.Instance.GetDynamicPrefabDecorator()
-        .GetPrefabFromWorldPos((int)prefabPos.x, (int)prefabPos.z);
-      if (prefabFromWorldPos is null) {
-        Log.Error(
-          $"[StrongUtils] Could not find prefabFromWorldPos, skipping: entityID: {entityID} prefabPos: {prefabPos}\n{Environment.StackTrace}");
-      }
-    }
-  }*/
-
   public class Initializer : IModApi {
     public void InitMod(Mod _modInstance) {
       Harmony harmony = new(_modInstance.Name);
