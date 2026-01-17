9c491739de4d7f93799ee2227a036c6839f92f24
str0ngh34rt <strongheart@strongheart.games>
2026-01-16 20:18:40 -0500

    Add RWGTools project

---
+++ RWGTools/HarmonyInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace RWGTools {
+  public class HarmonyInitializer : IModApi {
+    public void InitMod(Mod modInstance) {
+      Harmony harmony = new(modInstance.Name);
+      harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+  }
+}
---
+++ RWGTools/StickySelection.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using WorldGenerationEngineFinal;
+
+namespace RWGTools {
+  public class StickySelection {
+    public static XUiC_WorldGenerationWindowGroup PreviousWindowGroup;
+
+    public static void ApplyStickySelections(XUiC_WorldGenerationWindowGroup windowGroup) {
+      if (PreviousWindowGroup is null) {
+        return;
+      }
+      windowGroup.BiomeLayoutComboBox.Value = PreviousWindowGroup.BiomeLayoutComboBox.Value;
+    }
+  }
+
+  [HarmonyPatch(typeof(XUiC_WorldGenerationWindowGroup), nameof(XUiC_WorldGenerationWindowGroup.OnOpen))]
+  public class XUiC_WorldGenerationWindowGroup_OnOpen_Patch {
+    private static void Postfix(XUiC_WorldGenerationWindowGroup __instance) {
+      StickySelection.ApplyStickySelections(__instance);
+    }
+  }
+
+  [HarmonyPatch(typeof(XUiC_WorldGenerationWindowGroup), nameof(XUiC_WorldGenerationWindowGroup.OnClose))]
+  public class XUiC_WorldGenerationWindowGroup_OnClose_Patch {
+    private static void Prefix(XUiC_WorldGenerationWindowGroup __instance) {
+      StickySelection.PreviousWindowGroup = __instance;
+    }
+  }
+
+}
