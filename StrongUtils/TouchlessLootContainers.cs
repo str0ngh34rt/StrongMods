9edf71c5ed6d792b82dd1b92caa4c409222735d7
str0ngh34rt <strongheart@strongheart.games>
2026-05-18 20:57:51 -0400

    Add Touchless Loot Containers

---
+++ StrongUtils/TouchlessLootContainers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace StrongUtils {
+  public class TouchlessLootContainers {
+    [HarmonyPatch(typeof(TileEntityLootContainer), nameof(TileEntityLootContainer.UpdateTick))]
+    public class TileEntityLootContainer_UpdateTick_Patch {
+      private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+        CodeMatcher codeMatcher = new(instructions);
+        codeMatcher.MatchEndForward(
+            CodeMatch.Calls(() => GamePrefs.GetInt(EnumGamePrefs.LootRespawnDays)),
+            CodeMatch.Branches()
+          )
+          .ThrowIfInvalid("[StrongUtils] Could not find GamePrefs.GetInt() call")
+          .Advance(1)
+          .Insert(new CodeInstruction(OpCodes.Ret));
+        //Log.Out($"[StrongUtils] Instructions:\n    {string.Join("\n    ", codeMatcher.Instructions())}");
+        return codeMatcher.Instructions();
+      }
+    }
+  }
+}
