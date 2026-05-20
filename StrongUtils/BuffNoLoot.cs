81a6dfa484fb4685077dc58bd74f84ff1efd3d83
str0ngh34rt <strongheart@strongheart.games>
2026-05-20 13:59:48 -0400

    Add buff_no_loot and set it when in a no_hostiles zone.

---
+++ StrongUtils/BuffNoLoot.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace StrongUtils {
+  public class BuffNoLoot {
+    [HarmonyPatch(typeof(EntityAlive), nameof(EntityAlive.dropItemOnDeath))]
+    public class EntityAlive_dropItemOnDeath_Patch {
+      private static bool Prefix(ref EntityAlive __instance) {
+        return !__instance.Buffs.HasBuff("buff_no_loot");
+      }
+    }
+  }
+}
