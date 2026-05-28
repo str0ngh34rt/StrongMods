81fa2087efdaf2b05fbf2e4b28e9d9aa216c3098
str0ngh34rt <strongheart@strongheart.games>
2026-05-28 17:01:42 -0400

    Change patch to postfix

--- StrongFill/StrongFill.cs
+++ StrongFill/StrongFill.cs
@@ -6,43 +6,29 @@ namespace StrongFill {
     private const string FillBlock = "strong_fill";
 
     public static void FillIfPresent(List<BlockChangeInfo> changes) {
-      List<Vector3i> positionsToFill = null;
-      for (var x = 0; x < changes.Count; x++) {
-        BlockChangeInfo change = changes[x];
+      foreach (BlockChangeInfo change in changes) {
         if (change.blockValue.Block.blockName is not FillBlock) {
           continue;
         }
 
-        positionsToFill ??= new List<Vector3i>();
-        positionsToFill.Add(new Vector3i(change.pos.x, change.pos.y - 1, change.pos.z));
+        World world = GameManager.Instance.World;
 
-        changes.RemoveAt(x);
-      }
 
-      if (positionsToFill is null) {
-        return;
-      }
-
-      // Add at the end so we don't bother checking the new changes in the loop above
-      foreach (Vector3i position in positionsToFill) {
-        BlockValue block = GameManager.Instance.World.GetBlock(position);
-        if (block.isair || block.isWater || block.ischild || block.Block.AutoShapeType != EAutoShapeType.Shape) {
-          continue;
+        var position = new Vector3i(change.pos.x, change.pos.y - 1, change.pos.z);
+        BlockValue block = world.GetBlock(position);
+        if (block is { isair: false, isWater: false, ischild: false } &&
+            block.Block.AutoShapeType == EAutoShapeType.Shape) {
+          world.SetBlockRPC(position, block, MarchingCubes.DensityTerrainHi);
         }
 
-        changes.Add(new BlockChangeInfo {
-          pos = position,
-          bChangeDensity = true,
-          density = MarchingCubes.DensityTerrainHi,
-          bChangeBlockValue = false
-        });
+        world.SetBlockRPC(change.pos, BlockValue.Air);
       }
     }
   }
 
   [HarmonyPatch(typeof(GameManager), nameof(GameManager.ChangeBlocks))]
   public class GameManager_ChangeBlocks_Patch {
-    private static void Prefix(List<BlockChangeInfo> _blocksToChange) {
+    private static void Postfix(List<BlockChangeInfo> _blocksToChange) {
       StrongFill.FillIfPresent(_blocksToChange);
     }
   }
