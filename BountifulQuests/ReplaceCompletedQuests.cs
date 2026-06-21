0ab72c2919c505be44a9a155d775a85760dd9844
str0ngh34rt <strongheart@strongheart.games>
2026-06-21 10:41:48 -0400

    Hook for replacing completed quests

---
+++ BountifulQuests/ReplaceCompletedQuests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace BountifulQuests {
+  public class ReplaceCompletedQuests {
+
+  }
+
+  [HarmonyPatch(typeof(QuestEventManager), nameof(QuestEventManager.GetQuestList))]
+  public class QuestEventManager_GetQuestList_Patch {
+    private static void Postfix(ref QuestEventManager __instance, ref List<Quest> __result, World world, int npcEntityID, int playerEntityID) {
+      // TODO: Implement
+    }
+  }
+}
