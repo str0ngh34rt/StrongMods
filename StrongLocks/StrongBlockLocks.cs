f9a6724394b19caa10ac38e1050cafb6fa01226b
str0ngh34rt <strongheart@strongheart.games>
2026-06-29 07:47:52 -0400

    StrongLocks covers all lockable TEs as well as vehicles

---
+++ StrongLocks/StrongBlockLocks.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace StrongLocks {
+  [HarmonyPatch(typeof(GameManager), nameof(GameManager.ChangeBlocks))]
+  public class GameManager_ChangeBlocks_Patch {
+    private static void Prefix(List<BlockChangeInfo> _blocksToChange, out List<BlockValueRef> __state) {
+      __state = null;
+      foreach (BlockChangeInfo change in _blocksToChange) {
+        if (!(change.blockValue.Block is BlockCompositeTileEntity block &&
+              block.CompositeData.TryGetFeatureData<TEFeatureLockable>(out TileEntityFeatureData _))) {
+          continue;
+        }
+
+        BlockValue currentBlock = GameManager.Instance.World.GetBlock(change.blockValueRef);
+        if (change.blockValue.Equals(currentBlock)) {
+          continue;
+        }
+        __state ??= new List<BlockValueRef>();
+        __state.Add(change.blockValueRef);
+      }
+    }
+
+    private static void Postfix(List<BlockValueRef> __state) {
+      if (__state is null) {
+        return;
+      }
+      foreach (BlockValueRef block in __state) {
+        TileEntity tile = GameManager.Instance.World.GetTileEntity(block);
+        TEFeatureLockable lockable = tile?.GetSelfOrFeature<TEFeatureLockable>();
+        if (lockable is null) {
+          continue;
+        }
+
+        if (lockable.IsLocked()) {
+          continue;
+        }
+        lockable.SetLocked(true);
+      }
+    }
+  }
+}
--- StrongLocks/StrongEntityLocks.cs
+++ StrongLocks/StrongEntityLocks.cs
@@ -4,6 +4,7 @@ namespace StrongLocks {
   [HarmonyPatch(typeof(World), nameof(World.SpawnEntityInWorld))]
   public class World_SpawnEntityInWorld_Patch {
     private static void Postfix(Entity _entity) {
+      // Vehicles are the only player-owned entities that don't auto-lock
       if (_entity is not EntityVehicle vehicle) {
         return;
       }
