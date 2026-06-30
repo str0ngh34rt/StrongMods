dc09f47c9ff33db40ba2066bc0a87f0bf08bc671
str0ngh34rt <strongheart@strongheart.games>
2026-06-30 05:17:09 -0400

    Add SignDiagnostics

---
+++ StrongUtils/SignDiagnostics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HarmonyLib;
+
+namespace StrongUtils {
+  public class SignDiagnostics {
+    [HarmonyPatch(typeof(TEFeatureCanvas), nameof(TEFeatureCanvas.SetBlockEntityData))]
+    public class TEFeatureCanvas_SetBlockEntityData_Patch {
+      private static Exception Finalizer(TEFeatureCanvas __instance, Exception __exception) {
+        if (__exception is null) {
+          return null;
+        }
+
+        Block block = __instance.blockValue.Block;
+        Vector3i pos = __instance.ToWorldPos();
+        PrefabInstance poi = GameManager.Instance.World.GetPOIAtPosition(pos);
+        Log.Warning(
+          $"[SignDiagnostics] Could not load canvas block {block.blockName} at {pos} in {poi.prefab.LocalizedEnglishName} ({poi.prefab.PrefabName}):\n{__exception}");
+
+        return null;
+      }
+    }
+  }
+}
