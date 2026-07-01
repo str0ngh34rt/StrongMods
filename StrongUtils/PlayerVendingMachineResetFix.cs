1cbe83ee4c76472d624c7669a2dfa99a02b73d5d
str0ngh34rt <strongheart@strongheart.games>
2026-06-30 21:14:46 -0400

    Add player vending machine fix

---
+++ StrongUtils/PlayerVendingMachineResetFix.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace StrongUtils {
+  [HarmonyPatch(typeof(TraderInfo), nameof(TraderInfo.ResetInterval), MethodType.Getter)]
+  public class TraderInfo_get_ResetInterval_Patch {
+    private static bool Prefix(TraderInfo __instance, ref int __result) {
+      if (!__instance.PlayerOwned) {
+        return true;
+      }
+
+      __result = __instance.resetInterval;
+      return false;
+    }
+  }
+
+  [HarmonyPatch(typeof(TraderInfo), nameof(TraderInfo.ResetIntervalInTicks), MethodType.Getter)]
+  public class TraderInfo_get_ResetIntervalInTicks_Patch {
+    private static bool Prefix(TraderInfo __instance, ref int __result) {
+      if (!__instance.PlayerOwned) {
+        return true;
+      }
+
+      __result = __instance.resetIntervalInTicks;
+      return false;
+    }
+  }
+}
