7ae7e00a2680ee7eae74e4ad9894ef0985b1beb8
str0ngh34rt <strongheart@strongheart.games>
2026-07-13 17:20:34 -0400

    Auto-close trader doors

---
+++ AutoCloseDoors/AutoCloseDoors.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+
+namespace AutoCloseDoors {
+  public static class AutoCloseDoors {
+    private const string AutoCloseTag = "auto_close";
+    private const float MaxPlayerDistance = 10f;
+
+
+    private static void TryCloseDoor(TEFeatureDoor door, World world) {
+      if (!door.isOpen || !CanAutoClose(door)) {
+        return;
+      }
+
+      if (world.IsPlayerAliveAndNear(door.ToWorldPos(), MaxPlayerDistance)) {
+        Log.Out($"[AutoCloseDoors] Cannot auto-close door at {door.ToWorldPos()} because a player is nearby.");
+        return;
+      }
+
+      Log.Out($"[AutoCloseDoors] Auto-closing door at {door.ToWorldPos()}.");
+      door.SetOpen(false, true);
+    }
+
+    private static bool CanAutoClose(TEFeatureDoor door) {
+      return !door.blockValue.ischild && door.blockValue.Block.Tags.GetTagNames().Contains(AutoCloseTag) &&
+             GameManager.Instance.World.IsWithinTraderArea(door.ToWorldPos());
+    }
+
+    [HarmonyPatch(typeof(TEFeatureDoor), nameof(TEFeatureDoor.UpdateTick))]
+    public class TEFeatureDoor_UpdateTick_Patch {
+      private static void Postfix(TEFeatureDoor __instance, World world) {
+        if (world.IsRemote()) {
+          return;
+        }
+
+        TryCloseDoor(__instance, world);
+      }
+    }
+  }
+}
---
+++ AutoCloseDoors/README.md
@@ -0,0 +1,28 @@
+﻿# AutoCloseDoors
+
+Auto-close the main trader doors. Help prevent zombie ambushes in trader areas.
+
+* For now only works with the main trader doors (gates and chain-link fence doors).
+* Will not close if a player is within 10 meters of the door.
+
+## Installation
+
+* Copy the `AutoCloseDoors/` directory into `Mods/`
+* Make sure the `ModInfo.xml` appears one folder below `Mods/`, i.e. `Mods/AutoCloseDoors/ModInfo.xml`, otherwise the
+  mod
+  won't be loaded
+* Dedicated servers:
+  * Server-side only
+  * EAC-friendly
+* All other deployments:
+  * Deploy to host (in single-player this is your game)
+  * EAC must be disabled
+* There are no configuration options for now
+
+## Changelog
+
+### 1.0.0
+
+* Initial public release
+* Only works against 7DtD v3.x
+* Compiled and tested against 7DtD v3.0.1 b4
