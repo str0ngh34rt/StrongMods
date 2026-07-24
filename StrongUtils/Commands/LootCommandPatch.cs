07deaa9f9d35963ca9ffe36bc72511ed9d4dd8f8
str0ngh34rt <strongheart@strongheart.games>
2026-07-24 11:29:57 -0400

    Add patch for loot container command NRE

---
+++ StrongUtils/Commands/LootCommandPatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace StrongUtils.Commands {
+  // Fix for NRE using "loot container cars" console command
+  // https://community.thefunpimps.com/threads/nre-using-loot-container-cars-console-command.48387/
+  [HarmonyPatch(typeof(ConsoleCmdLoot), nameof(ConsoleCmdLoot.ContainerList))]
+  public static class LootCommandPatch {
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+      CodeMatcher codeMatcher = new(instructions);
+      codeMatcher.MatchStartForward(
+          new CodeMatch(OpCodes.Ldnull),
+          new CodeMatch(OpCodes.Ldc_I4_0),
+          CodeMatch.Calls(AccessTools.Method(typeof(LootContainer), nameof(LootContainer.SpawnLootItemsFromList)))
+        )
+        .ThrowIfInvalid("[LootCommandPatch] Could not find ldnull and SpawnLootItemsFromList() call")
+        .RemoveInstruction()
+        .Insert(
+          new CodeInstruction(OpCodes.Newobj, AccessTools.Constructor(typeof(List<string>)))
+        );
+      //Log.Out($"[LootCommandPatch] Instructions:\n    {string.Join("\n    ", codeMatcher.Instructions())}");
+      return codeMatcher.Instructions();
+    }
+  }
+}
